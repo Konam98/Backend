004eddef1fef29ee8a546288e333611c01d29033
Konam98 <85475408+Konam98@users.noreply.github.com>
2022-02-13 23:05:41 +0530

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -84,9 +84,16 @@ namespace ContactApplication
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Contact Application"));
+            //     app.UseSwagger();
+            //     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Contact Application"));
+            // 
             }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => 
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Contact Application");
+            });
+           
 
              app.UseCors(x => x
             .AllowAnyOrigin()
